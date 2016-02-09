74b63204f70305a952c2d145514f49b94eb8ec62
Johannes Meyer <johannes.meyer@netways.de>
2016-02-09 22:17:21 +0100

    Introduce package lib/elasticarmor

---
+++ lib/elasticarmor/__init__.py
@@ -0,0 +1,14 @@
+# ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
+
+VERSION = '0.9'
+APP_NAME = 'ElasticArmor'
+SYSLOG_DATE_FORMAT = '%b %e %H:%M:%S'
+SYSLOG_FORMAT ='%(asctime)s %(name)s: %(message)s'
+FILE_LOG_FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
+FILE_LOG_FORMAT_DEBUG = '%(asctime)s - %(process)d:%(threadName)s:%(name)s - %(levelname)s - %(message)s'
+
+DEFAULT_CONFIG = '/etc/elasticarmor/config.ini'
+DEFAULT_LOGFILE = '/var/log/elasticarmor/elasticarmor.log'
+DEFAULT_NODE = 'localhost:9200'
+DEFAULT_ADDRESS = 'localhost'
+DEFAULT_PORT = 59200
