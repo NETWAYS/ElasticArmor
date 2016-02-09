797e83c21b8e183319cd1c1620889b664f4cb542
Johannes Meyer <johannes.meyer@netways.de>
2016-02-09 22:20:50 +0100

    Introduce module lib/elasticarmor/util/mixins

---
+++ lib/elasticarmor/util/mixins.py
@@ -0,0 +1,18 @@
+# ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
+
+import logging
+
+__all__ = ['LoggingAware']
+
+
+class LoggingAware:
+    """Logging mixin which provides a convenience property for lazy logging."""
+
+    @property
+    def log(self):
+        """Return a logger instance properly set up for the class utilizing this mixin."""
+        try:
+            return self.__class__.__log
+        except AttributeError:
+            self.__class__.__log = logging.getLogger(self.__module__)
+            return self.__class__.__log
