c921594a8743c6f103bef58329cb6c6d30120922
Johannes Meyer <johannes.meyer@netways.de>
2016-02-09 22:19:46 +0100

    Introduce module lib/elasticarmor/util/config

---
+++ lib/elasticarmor/util/config.py
@@ -0,0 +1,23 @@
+# ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
+
+import ConfigParser
+
+
+class Parser(ConfigParser.RawConfigParser):
+    """INI Parser derived from ConfigParser.RawConfigParser which is
+    aware of default values when accessing a non-existent option."""
+
+    def get(self, section, option):
+        """Return the value of the given option located in the given section.
+
+        In contrast to ConfigParser.RawConfigParser.get this will not raise ConfigParser.NoSectionError
+        or ConfigParser.NoOptionError if there is a default value configured for the given option.
+        """
+
+        try:
+            return ConfigParser.RawConfigParser.get(self, section, option)
+        except ConfigParser.NoSectionError as error:
+            try:
+                return self._defaults[self.optionxform(option)]
+            except KeyError:
+                raise error
