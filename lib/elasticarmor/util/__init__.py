2e531896fe29f2eb129ed03c4f484974e44ec0aa
Johannes Meyer <johannes.meyer@netways.de>
2016-02-12 10:52:26 +0100

    util: Introduce decorator @classproperty
    refs #3037

--- lib/elasticarmor/util/__init__.py
+++ lib/elasticarmor/util/__init__.py
@@ -1,1 +1,22 @@
 # ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
+
+
+class classproperty(object):
+    """Dead-simple @classproperty decorator
+
+    Usage:
+        class Foo(object):
+            @classproperty
+            def bar(cls):
+                return cls
+
+        assert Foo.bar is Foo
+        assert Foo().bar is Foo
+
+    Original solution by Denis Ryzhkov @ http://stackoverflow.com/a/13624858."""
+
+    def __init__(self, func):
+        self.func = func
+
+    def __get__(self, instance, owner):
+        return self.func(owner)
