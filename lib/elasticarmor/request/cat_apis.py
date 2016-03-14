b1a82185050ee288666f15863f0a0c8bcaaa436c
Johannes Meyer <johannes.meyer@netways.de>
2016-03-14 16:25:35 +0100

    elasticarmor.request: Introduce class CatApiRequest
    resolves #3045

---
+++ lib/elasticarmor/request/cat_apis.py
@@ -0,0 +1,15 @@
+# ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
+
+from elasticarmor.request import *
+
+
+class CatApiRequest(ElasticRequest):
+    # Consider this as a placeholder. The cat API is way more comprehensive than is_valid may convey.
+    # This request handler should be superseded by more sophisticated solutions in the future.
+
+    def is_valid(self):
+        return self.path.startswith('/cat')
+
+    @Permission('api/cat')
+    def inspect(self, client):
+        pass
