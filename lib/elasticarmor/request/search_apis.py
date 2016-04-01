da25822ad0eab02b3ee7885b3316c288f3725b69
Johannes Meyer <johannes.meyer@netways.de>
2016-04-01 15:43:33 +0200

    SearchApiRequest: Enforce restrictions on the URL
    refs #3047

--- lib/elasticarmor/request/search_apis.py
+++ lib/elasticarmor/request/search_apis.py
@@ -1,6 +1,8 @@
 # ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+
 
+from elasticarmor.auth import MultipleIncludesError
 from elasticarmor.request import *
+from elasticarmor.util.elastic import SourceFilter, FilterString
 
 
 class SearchApiRequest(ElasticRequest):
@@ -17,9 +19,94 @@ class SearchApiRequest(ElasticRequest):
         ]
     }
 
-    @Permission('api/search/documents')
     def inspect(self, client):
-        pass
+        index_filter, type_filter, source_filter, json = self.inspect_request(
+            client, FilterString.from_string(self.get_match('indices', '')),
+            FilterString.from_string(self.get_match('documents', '')),
+            SourceFilter.from_query(self.query), self.json)
+
+        if not self.query.is_false('explain'):
+            for index in index_filter.iter_patterns():
+                if type_filter:
+                    for document_type in type_filter.iter_patterns():
+                        if not client.can('api/search/explain', str(index), str(document_type)):
+                            raise PermissionError(
+                                'You are not permitted to access scoring explanations of the given indices or types.')
+                elif not client.can('api/search/explain', str(index)):
+                    raise PermissionError('You are not permitted to access scoring explanations of the given indices.')
+
+            if not index_filter and not client.can('api/search/explain'):
+                raise PermissionError('You are not permitted to access scoring explanations.')
+
+        if index_filter and type_filter and self.query.get('fields'):
+            fields = filter(None, (field.strip() for v in self.query['fields'] for field in v.split(',')))
+            forbidden_fields = []
+            for index in index_filter.iter_patterns():
+                for document_type in type_filter.iter_patterns():
+                    for field in fields:
+                        if not client.can('api/documents/get', str(index), str(document_type), field):
+                            forbidden_fields.append(field)
+
+            if forbidden_fields:
+                raise PermissionError('You are not permitted to access the following fields: {0}'
+                                      ''.format(', '.join(forbidden_fields)))
+
+        if index_filter:
+            if type_filter:
+                self.path = '/{0}/{1}/_search'.format(index_filter, type_filter)
+            else:
+                self.path = '/{0}/_search'.format(index_filter)
+
+        if source_filter:
+            self.query.discard('_source', '_source_include', '_source_exclude')
+            self.query.update(source_filter.as_query())
+
+        if json is not None:
+            self.body = self.json_encode(json)
+
+    @staticmethod
+    def inspect_request(client, requested_indices, requested_types, requested_source, json=None):
+        restricted_types = client.is_restricted('types')
+
+        try:
+            index_filter = client.create_filter_string('api/search/documents', requested_indices,
+                                                       single=restricted_types)
+        except MultipleIncludesError as error:
+            raise PermissionError(
+                'You are restricted to specific types or fields. To use the search api, please pick'
+                ' a single index from the following list: {0}'.format(', '.join(error.includes)))
+        else:
+            if index_filter is None:
+                raise PermissionError('You are not permitted to search for documents in the given indices.')
+
+        if restricted_types:
+            restricted_fields = client.is_restricted('fields')
+            requested_index = index_filter.combined[0] if index_filter.combined else index_filter[0]
+
+            try:
+                type_filter = client.create_filter_string('api/search/documents', requested_types,
+                                                          str(requested_index), restricted_fields)
+            except MultipleIncludesError as error:
+                raise PermissionError(
+                    'You are restricted to specific fields. To use the search api, please pick a'
+                    ' single type from the following list: {0}'.format(', '.join(error.includes)))
+            else:
+                if type_filter is None:
+                    raise PermissionError('You are not permitted to search for documents of the given types.')
+
+            if restricted_fields:
+                requested_type = type_filter.combined[0] if type_filter.combined else type_filter[0]
+                source_filter = client.create_source_filter('api/documents/get', str(requested_index),
+                                                            str(requested_type), requested_source)
+                if source_filter is None:
+                    raise PermissionError('You are not permitted to access the requested document and/or fields.')
+            else:
+                source_filter = requested_source
+        else:
+            type_filter = requested_types
+            source_filter = requested_source
+
+        return index_filter, type_filter, source_filter, None
 
 
 class SearchTemplateApiRequest(ElasticRequest):
