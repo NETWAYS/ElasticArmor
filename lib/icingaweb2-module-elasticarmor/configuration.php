6ea94539fd067c1aedd06974e3800bc2c8b94ebc
Johannes Meyer <johannes.meyer@netways.de>
2016-04-29 13:19:41 +0200

    Introduce icingaweb2-module-elasticarmor
    refs #3055

---
+++ lib/icingaweb2-module-elasticarmor/configuration.php
@@ -0,0 +1,7 @@
+<?php
+/* ElasticArmor | (c) 2016 NETWAYS GmbH | GPLv2+ */
+
+/** @var $this \Icinga\Application\Modules\Module */
+
+$menuSection = $this->menuSection('ElasticArmor')
+    ->setIcon('lock');
